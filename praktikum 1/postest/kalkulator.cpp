ba14629ec14cb81225b0445f8f6a34b638f48bff
Bagus Viki <vikiamalindo7@gmail.com>
2022-03-20 15:38:18 +0700

    membua kalkulator2022-cpp

---
+++ praktikum 1/postest/kalkulator.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int main ()
+{
+	int a, b, hasil, menu ;
+
+	do{
+		
+	
+	
+	printf("\nProgram kalkulator sederhana\n");
+	printf("============================");
+	printf("\n Menu pilihan :");
+	printf("\n 1.Penjumlahan ");
+	printf("\n 2.Pengurangan ");
+	printf("\n 3.Perkalian ");
+	printf("\n 4.pembagian ");
+	printf("\n 0.exit\n");
+	printf("Pilihan menu :");
+	scanf("%d", &menu);
+	printf("===========================");
+	switch (menu){
+		}
+	}
+}
